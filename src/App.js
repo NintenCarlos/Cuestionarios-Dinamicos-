5814008a8748aee2aa149b851b176c600e7f7e27
NIntenCarlos <juancarlossantoyo0@gmail.com>
2024-10-05 21:09:49 -0600

    Cuestionarios dinámicos

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+import { Questions } from "./Components/Questions/Questions";
+
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <Questions />
+      </Container>
+    </>
+  );
+};
+
+export default App;
